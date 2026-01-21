b4ae87ef759f69a5766ac9038915077a64cc15b2
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2026-01-21 15:22:50 +0530

    just commit

---
+++ examples/example5.js
@@ -0,0 +1,8 @@
+let x = "10";
+let y = 10;
+
+console.log(x==y);  // it'll check only value
+console.log(x===y); // it'll check both value and datatype
+
+console.log(x!=y);  // it'll check only value
+console.log(x!==y); // it'll check both value and datatype
