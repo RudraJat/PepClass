c3fbfd713ac4373b358c0940f0ceec8623d72803
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2026-01-22 10:45:22 +0530

    arrow function

---
+++ examples/example12.js
@@ -0,0 +1,9 @@
+//arrow function
+//when we were declarin function we we adding function name twice so to avoid that we have arrow function
+
+const add =(a,b)=>{
+    return Number(a)+Number(b);
+}
+
+const r = add(5,19);
+console.log(r);
