567c94cf566c90481cae7ed8a4272cdcd4c603d7
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2026-01-22 10:18:38 +0530

    functions

---
+++ examples/example11.js
@@ -0,0 +1,22 @@
+//thumb rule: make a function only if you are going to repeat the code more than once
+
+// function addNumber(a,b){
+//     return Number(a)+Number(b);
+// }
+
+// let sum = addNumber("rudra",10);
+// console.log(sum);//NaN
+
+// sum = addNumber("20",30);
+// console.log(sum);//50
+
+function addNumber(a,b){
+    a+=2;
+    b+=3;
+    return Number(a)+Number(b);
+}
+
+let x=12;
+let y=13;
+
+
